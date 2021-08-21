7cb836b42364ace7ee2e34ab1e4fc49c2e3f8ebc
Ananna14 <anannasheikh89@gmail.com>
2021-08-21 04:41:48 -0700

    first-commit

--- app.js
+++ app.js
@@ -1,60 +1,98 @@
 // Extra Memory Cost 
 function update(isInteger) {
-    caseInput = document.getElementById('case-number');
-    caseText = caseInput.innerText;
-    caseInput.innerText = parseInt(caseText);
+    const memoryInput = document.getElementById('case-number');
+    let memoryNumber = memoryInput.innerText;
+    memoryInput.innerText = parseInt(memoryNumber);
     if (isInteger) {
-        caseInput.innerText = 0;
+        memoryNumber = 0;
     }
     else {
-        caseInput.innerText = 180;
+        memoryNumber = 180;
     }
+    memoryInput.innerText = memoryNumber;
+    // update-total 
+    const totalInput = document.getElementById('total-price');
+    totalInput.innerText = memoryNumber + 1299;
+    // total 
+    const subTotal = document.getElementById('sub-total');
+    subTotal.innerText = memoryNumber + 1299;
 }
-
-// Extra Memory Cost 
 document.getElementById('case-input').addEventListener('click', function () {
     update(true);
+
 });
 document.getElementById('case-button').addEventListener('click', function () {
     update(false);
+
 });
+
 
 
 
 
 // Extra Storage Cost 
-document.getElementById('second-case').addEventListener('click', function () {
-    storageInput = document.getElementById('second-number');
-    storageNumber = storageInput.innerText;
-    storageInput.innerText = parseInt(storageNumber);
-    storageInput.innerText = 0;
-});
-document.getElementById('third-case').addEventListener('click', function () {
-    storageInput = document.getElementById('second-number');
-    storageNumber = storageInput.innerText;
-    storageInput.innerText = parseInt(storageNumber);
-    storageInput.innerText = 100;
-});
-document.getElementById('fourth-case').addEventListener('click', function () {
-    storageInput = document.getElementById('second-number');
-    storageNumber = storageInput.innerText;
-    storageInput.innerText = parseInt(storageNumber);
-    storageInput.innerText = 180;
-});
+
+// document.getElementById('second-case').addEventListener('click', function () {
+//     const storageInput = document.getElementById('second-number');
+//     let storageNumber = storageInput.innerText;
+//     storageInput.innerText = parseInt(storageNumber);
+//     storageNumber = 0;
+// });
+// document.getElementById('third-case').addEventListener('click', function () {
+//     const storageInput = document.getElementById('second-number');
+//     let storageNumber = storageInput.innerText;
+//     storageInput.innerText = parseInt(storageNumber);
+//     storageNumber = 100;
+// });
+// document.getElementById('fourth-case').addEventListener('click', function () {
+//     const storageInput = document.getElementById('second-number');
+//     let storageNumber = storageInput.innerText;
+//     storageInput.innerText = parseInt(storageNumber);
+//     storageNumber = 180;
+// });
+// // update-total 
+// const totalInput = document.getElementById('total-price');
+// totalInput.innerText = storageNumber + 1299;
+// // total 
+// const subTotal = document.getElementById('sub-total');
+// subTotal.innerText = storageNumber + 1299;
 
 
 
 
 // Delivery Charge 
-document.getElementById('delivery-input').addEventListener('click', function () {
-    deliveryInput = document.getElementById('third-number');
-    deliveryNumber = deliveryInput.innerText;
+
+function delivery(cost) {
+    const deliveryInput = document.getElementById('third-number');
+    let deliveryNumber = deliveryInput.innerText;
     deliveryInput.innerText = parseInt(deliveryNumber);
-    deliveryInput.innerText = 0;
+    if (cost) {
+        deliveryNumber = 0;
+    }
+    else {
+        deliveryNumber = 20;
+    }
+    deliveryInput.innerText = deliveryNumber;
+    // update-total
+    const totalInput = document.getElementById('total-price');
+    totalInput.innerText = deliveryNumber + 1299;
+    // total 
+    const subTotal = document.getElementById('sub-total');
+    subTotal.innerText = deliveryNumber + 1299;
+}
+
+document.getElementById('delivery-input').addEventListener('click', function () {
+    delivery(true);
 })
 document.getElementById('delivery-case').addEventListener('click', function () {
-    deliveryInput = document.getElementById('third-number');
-    deliveryNumber = deliveryInput.innerText;
-    deliveryInput.innerText = parseInt(deliveryNumber);
-    deliveryInput.innerText = 20;
+    delivery(false);
 });
+
+
+// input 
+
+
+
+
+
+
