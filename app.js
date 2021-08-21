d529d434e6d8dcb225061dd88e8fa72ec8c860a3
Ananna14 <anannasheikh89@gmail.com>
2021-08-21 08:40:37 -0700

    fourth-commit

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ function update(isInteger) {
     }
     memoryInput.innerText = memoryNumber;
     // update-total 
-    const totalInput = document.getElementById('total-price');
+    const totalInput = document.getElementById('totalPrice');
     totalInput.innerText = memoryNumber + 1299;
     // total 
-    const subTotal = document.getElementById('sub-total');
+    const subTotal = document.getElementById('total');
     subTotal.innerText = memoryNumber + 1299;
 }
 document.getElementById('case-input').addEventListener('click', function () {
@@ -37,11 +37,11 @@ document.getElementById('second-case').addEventListener('click', function () {
     storageInput.innerText = 0;
 
     // update-total
-    const totalInput = document.getElementById('total-price');
+    const totalInput = document.getElementById('totalPrice');
     totalInput.innerText = parseInt(storageNumber) + 1299;
 
     // total 
-    const subTotal = document.getElementById('sub-total');
+    const subTotal = document.getElementById('total');
     subTotal.innerText = parseInt(storageNumber) + 1299;
 
 });
@@ -51,11 +51,11 @@ document.getElementById('third-case').addEventListener('click', function () {
     storageInput.innerText = 100;
 
     // update-total
-    const totalInput = document.getElementById('total-price');
+    const totalInput = document.getElementById('totalPrice');
     totalInput.innerText = parseInt(storageNumber) + 1299;
 
     // total 
-    const subTotal = document.getElementById('sub-total');
+    const subTotal = document.getElementById('total');
     subTotal.innerText = parseInt(storageNumber) + 1299;
 });
 document.getElementById('fourth-case').addEventListener('click', function () {
@@ -64,18 +64,14 @@ document.getElementById('fourth-case').addEventListener('click', function () {
     storageInput.innerText = 180;
 
     // update-total
-    const totalInput = document.getElementById('total-price');
+    const totalInput = document.getElementById('totalPrice');
     totalInput.innerText = parseInt(storageNumber) + 1299;
 
     // total 
-    const subTotal = document.getElementById('sub-total');
+    const subTotal = document.getElementById('total');
     subTotal.innerText = parseInt(storageNumber) + 1299;
 
 });
-
-
-
-
 
 
 // Delivery Charge 
@@ -92,10 +88,10 @@ function delivery(cost) {
     }
     deliveryInput.innerText = deliveryNumber;
     // update-total
-    const totalInput = document.getElementById('total-price');
+    const totalInput = document.getElementById('totalPrice');
     totalInput.innerText = deliveryNumber + 1299;
     // total 
-    const subTotal = document.getElementById('sub-total');
+    const subTotal = document.getElementById('total');
     subTotal.innerText = deliveryNumber + 1299;
 }
 
@@ -107,7 +103,18 @@ document.getElementById('delivery-case').addEventListener('click', function () {
 });
 
 
-// input 
+// update promo code
+document.getElementById("promoCodeBtn").addEventListener("click", function () {
+    let discount = document.getElementById("promoCode").value.trim();
+
+    if (discount === 'stevekaku') {
+        let totalCost = document.getElementById('totalPrice').innerText;
+        let cost = (totalCost / 100) * 20;
+        console.log(cost);
+        document.getElementById('total').innerText = parseFloat(totalCost - cost);
+    }
+    document.getElementById("promoCode").value = ' ';
+});
 
 
 
